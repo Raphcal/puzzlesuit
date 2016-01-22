0fe1b3b31541755ee973bf97696d2929de620048
Raphaël <raphael@Alu.local>
2016-01-22 22:19:36 +0100

    Initial Commit

---
+++ puzzlesuit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  puzzlesuit
+//
+//  Created by Raphaël Calabro on 22/01/2016.
+//  Copyright © 2016 Raphaël Calabro. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ puzzlesuit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  puzzlesuit
+//
+//  Created by Raphaël Calabro on 22/01/2016.
+//  Copyright © 2016 Raphaël Calabro. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
