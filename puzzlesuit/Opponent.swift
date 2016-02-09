7b6635bf37274b0e0f114e00be89d0f1e3ff50c9
Raphaël <raphael@Alu.local>
2016-02-09 22:07:01 +0100

    Ajout d'un enum pour les adversaires.

---
+++ puzzlesuit/Opponent.swift
@@ -0,0 +1,24 @@
+//
+//  Opponent.swift
+//  PuzzleSuit
+//
+//  Created by Raphaël Calabro on 09/02/2016.
+//  Copyright © 2016 Raphaël Calabro. All rights reserved.
+//
+
+import Foundation
+
+enum Opponent {
+    
+    case First
+    case Second
+    case Third
+    case Fourth
+    case Fifth
+    case Sixth
+    case Seventh
+    case Eighth
+    case Nineth
+    case Tenth
+    
+}
